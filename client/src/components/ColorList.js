112cb9c60d6e13c65f290f34a8ca54e0aed0f959
TheIllusiveC4 <gasingdong@gmail.com>
2019-08-23 09:37:25 -0700

    update colors

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -16,11 +17,17 @@ const ColorList = ({ colors, updateColors }) => {
     setColorToEdit(color);
   };
 
-  const saveEdit = e => {
+  const saveEdit = async (e) => {
     e.preventDefault();
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    try {
+      await axiosWithAuth().put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit);
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const deleteColor = color => {
@@ -32,7 +39,7 @@ const ColorList = ({ colors, updateColors }) => {
       <p>colors</p>
       <ul>
         {colors.map(color => (
-          <li key={color.color} onClick={() => editColor(color)}>
+          <li key={color.color}>
             <span>
               <span className="delete" onClick={() => deleteColor(color)}>
                 x
@@ -42,6 +49,7 @@ const ColorList = ({ colors, updateColors }) => {
             <div
               className="color-box"
               style={{ backgroundColor: color.code.hex }}
+              onClick={() => editColor(color)}
             />
           </li>
         ))}
