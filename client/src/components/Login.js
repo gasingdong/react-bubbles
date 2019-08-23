be69a3260a98c84b63e56263c1d21b648f9969d4
TheIllusiveC4 <gasingdong@gmail.com>
2019-08-23 08:49:50 -0700

    build login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,10 +4,19 @@ const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
-    <>
+    <div className="login-form">
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <form>
+        <label htmlFor="username">
+          Username:
+          <input type="text" name="username" placeholder="JohnDoe" />
+        </label>
+        <label htmlFor="password">
+          Password:
+          <input type="text" name="password" placeholder="123456" />
+        </label>
+      </form>
+    </div>
   );
 };
 
